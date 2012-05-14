88281ad637747d45c641dca8a75f71708443eb70
novo.dimaporo <novo.dimaporo@orangeandbronze.com>
2012-05-14 20:14:13 +0800

    working receive and send, yes

--- src/com/onb/eventHowler/service/EventHowlerBroadcastReceiver.java
+++ src/com/onb/eventHowler/service/EventHowlerBroadcastReceiver.java
@@ -1,10 +1,11 @@
 package com.onb.eventHowler.service;
 
 import com.onb.eventHowler.application.EventHowlerOpenDbHelper;
+import com.onb.eventHowler.domain.EventHowlerParticipant;
+
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.database.Cursor;
 import android.os.Bundle;
 import android.telephony.SmsMessage;
 import android.util.Log;
@@ -18,26 +19,32 @@ public class EventHowlerBroadcastReceiver extends BroadcastReceiver{
 		
 		EventHowlerOpenDbHelper openHelper = new EventHowlerOpenDbHelper(context);
 		
-		//test
-		Cursor participant = openHelper.getAllParticipant();
-		Log.d("broadcastReceiver", participant.getCount() + " broadcast receiver");
-		participant.close();
-		//test
-		
 	        Bundle bundle = intent.getExtras();        
 	        SmsMessage[] msgs = null;
 	        String str = "";
+	        String name;
 	        if (bundle != null)
 	        {
-
 	            Object[] pdus = (Object[]) bundle.get("pdus");
 	            msgs = new SmsMessage[pdus.length];            
 	            for (int i=0; i<msgs.length; i++){
-	                msgs[i] = SmsMessage.createFromPdu((byte[])pdus[i]);
-	                str += "SMS from " + msgs[i].getOriginatingAddress();                     
-	                str += " :";
-	                str += msgs[i].getMessageBody().toString();
-	                str += "\n";        
+	            	
+	            	msgs[i] = SmsMessage.createFromPdu((byte[])pdus[i]);
+	            	name = openHelper.findNumber(msgs[i].getOriginatingAddress());
+	            	Log.d("onReceiver", msgs[i].getOriginatingAddress());
+	            	
+	            	if(name != "NONE"){
+	            		
+	            		openHelper.updateStatus(new EventHowlerParticipant(name,
+	            				msgs[i].getDisplayOriginatingAddress(),
+	            				msgs[i].getDisplayMessageBody()));
+	            		
+		                str += "SMS from " + msgs[i].getOriginatingAddress();                     
+		                str += " :";
+		                str += msgs[i].getMessageBody().toString();
+		                str += "\n";
+		                Log.d("broadcastReceiver", "receive message from " + msgs[i].getOriginatingAddress());
+	            	}
 	            }
 
 	            Toast.makeText(context, str, Toast.LENGTH_SHORT).show(); //for test

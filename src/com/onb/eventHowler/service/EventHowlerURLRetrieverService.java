cf21a32eacf33766e972d247b58952fd1a3239f7
xander <xander@asus1.manila.orangeandbronze.com>
2012-05-15 17:42:45 +0800

    Added url generation for queries and appending to database using event ID and secret key

---
+++ src/com/onb/eventHowler/service/EventHowlerURLRetrieverService.java
@@ -0,0 +1,97 @@
+package com.onb.eventHowler.service;
+
+import java.util.List;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.onb.eventHowler.application.EventHowlerJSONHelper;
+import com.onb.eventHowler.application.EventHowlerOpenDbHelper;
+import com.onb.eventHowler.domain.EventHowlerParticipant;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class EventHowlerURLRetrieverService extends Service{
+
+	private static EventHowlerOpenDbHelper openHelper;
+	private static final String QUERY_URL_FORMAT = "EventHowlerApp/query?id=%ssecretKey=%s";
+	
+	@Override
+	public void onCreate() {
+		openHelper = new EventHowlerOpenDbHelper(getApplicationContext());
+	}
+	
+	@Override
+	public IBinder onBind(Intent intent) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	/**
+	 * Retrieves participant data from the web application 
+	 * using the corresponding event ID and secret key
+	 * 
+	 * @param id			unique event id
+	 * @param secretKey		corresponding event key
+	 */
+	public void retrieveAndStoreParticipantsFromIdAndKey(String id, String secretKey){
+		String query = generateQueryURL(id, secretKey);
+		retrieveAndStoreParticipantListFromURL(query);
+	}
+	
+	/**
+	 * Generates a URL for querying from the web application
+	 * 
+	 * @param id			unique event id
+	 * @param secretKey		corresponding event key
+	 * @return				generated query URL
+	 */
+	public String generateQueryURL(String id, String secretKey) {
+		return String.format(QUERY_URL_FORMAT, id, secretKey);
+	}
+
+	/**
+	 * Retrieves a JSON formatted String from a specified String url.
+	 * Stores the participant data from the JSON formatted String into
+	 * the local database.
+	 * 
+	 * @param url location of webpage to retrieve and store data from
+	 */
+	public void retrieveAndStoreParticipantListFromURL(String url)
+	{
+		List<JSONArray> list = EventHowlerJSONHelper.extractFromURL(url);
+		
+		for(JSONArray jArray: list) {
+			for(int i = 0; i < jArray.length(); i++){
+				try {
+					JSONObject jObject = jArray.getJSONObject(i);
+					storeAsParticipant(jObject);
+				} catch (JSONException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+	
+	/**
+	 * Creates an EventHowlerParticipant object from a JSONObject and
+	 * stores it as a row in the database.
+	 * 
+	 * @param jObject		JSONObject to be stored as an EventHowlerParticipant
+	 * @throws JSONException
+	 */
+	public void storeAsParticipant(JSONObject jObject) throws JSONException
+	{
+		EventHowlerParticipant participant = EventHowlerJSONHelper.convertJSONObjectToParticipant(jObject);
+		if(openHelper.checkNumberIfExist(participant.getPhoneNumber())) {
+			openHelper.updateStatus(participant, "");
+		}
+		else {
+			openHelper.insertParticipant(participant);	
+		}
+	}
+}
